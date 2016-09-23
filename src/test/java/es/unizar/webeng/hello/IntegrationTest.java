f6aa0e2da75b1d3bc794bdfb395063e82f4c9060
Jorge Sanz <jorge.sanz.alcaine@gmail.com>
2016-09-23 18:14:37 +0200

    Integration test 3

--- src/test/java/es/unizar/webeng/hello/IntegrationTest.java
+++ src/test/java/es/unizar/webeng/hello/IntegrationTest.java
@@ -22,9 +22,9 @@ public class IntegrationTest {
 
 
 	/**
-	* This class is used to check if the messages that are send to the server contain
-	* what they supposed to have. Integration test should be used after Unit test because
-	* it tests software modules (witch are alredy correct) as a group.
+	 This class is used to check if the messages that are send to the server contain
+	 what they supposed to have. Integration test should be used after Unit test because
+	 it tests software modules (witch are alredy correct) as a group.
 	*/
 
 
@@ -51,11 +51,11 @@ public class IntegrationTest {
 	}
 	
 	/**
-	 *  Send a GET message to the server located in localhost and port 0 with the path 
-	 *  "/webjars/bootstrap/3.3.5/css/bootstrap.min.css" and check if 
-	 *  the server response is OK (200). It also checks if the response contains 
-	 *  the text "body", if that´s the case, shows a warming message. If the specified media 
-	 *  type in the headers is not "text/css", show a warming message.
+	   Send a GET message to the server located in localhost and port 0 with the path 
+	   "/webjars/bootstrap/3.3.5/css/bootstrap.min.css" and check if 
+	   the server response is OK (200). It also checks if the response contains 
+	   the text "body", if that´s the case, shows a warming message. If the specified media 
+	   type in the headers is not "text/css", show a warming message.
 	 * 
 	 */
 	@Test
