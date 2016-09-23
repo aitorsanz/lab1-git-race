0abd74d8b9f5435f0d2739bbde55fd99c45f4d59
Jorge Sanz <jorge.sanz.alcaine@gmail.com>
2016-09-23 17:56:52 +0200

    Integration test documentation

--- src/test/java/es/unizar/webeng/hello/IntegrationTest.java
+++ src/test/java/es/unizar/webeng/hello/IntegrationTest.java
@@ -20,10 +20,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @DirtiesContext
 public class IntegrationTest {
 
+
+	/**
+	* This class is used to check if the messages that are send to the server contain
+	* what they supposed to have. Integration test should be used after Unit test because
+	* it tests software modules (witch are alredy correct) as a group.
+	*/
+
 	@Value("${local.server.port}")
 	private int port = 0;
 	
-	/**
+	
 	 * The objective of this test is to check the connection 
 	 * to the Home page. This method will throw an exception 
 	 * if the connection fails or if the body of the page 
@@ -42,6 +49,14 @@ public class IntegrationTest {
 				.getBody().contains("<title>Hello"));
 	}
 	
+	/**
+	 *  Send a GET message to the server located in localhost and port 0 with the path 
+	 *  "/webjars/bootstrap/3.3.5/css/bootstrap.min.css" and check if 
+	 *  the server response is OK (200). It also checks if the response contains 
+	 *  the text "body", if that´s the case, shows a warming message. If the specified media 
+	 *  type in the headers is not "text/css", show a warming message.
+	 * 
+	 */
 	@Test
 	public void testCss() throws Exception {
 		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
